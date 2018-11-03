0fb931797b508a0693d9014c8ecba1becd414cd4
Logan Geefs <lgeefs@gmail.com>
2018-11-03 14:44:58 -0700

    .

--- connect.py
+++ connect.py
@@ -1,12 +1,16 @@
 import os
 import sys
 import requests
+import csv
 
-res = requests.get("http://127.0.0.1/algo_trader/api/get/historical_prices?symbol=aapl")
+res = requests.get("http://127.0.0.1/algo_trader/api/get/historical_prices?symbol=aapl&range=5y")
+res_json = res.json()
 
-values = []
+if len(res_json) < 1:
+    exit
 
-for j in reversed(res.json()):
-    print(j)
-    break
-    
+with open('test.csv', 'w') as file:
+    csv_file = csv.writer(file)
+    csv_file.writerow(res_json[0].keys())
+    for item in res_json:
+        csv_file.writerow(item.values())
